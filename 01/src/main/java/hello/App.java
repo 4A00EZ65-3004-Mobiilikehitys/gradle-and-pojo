3393d49f58dabbe7fd003f733a2f06745524c51e
pohjus <jussi.pohjolainen@gmail.com>
2023-12-12 07:30:09 +0200

    Add template for 01

---
+++ 01/src/main/java/hello/App.java
@@ -0,0 +1,26 @@
+package hello;
+
+/**
+ * Gradle exercise.
+ *
+ * @author Firstname Lastname
+ */
+public class Main {
+
+    /**
+     * Prevent that instantiation is not possible.
+     */
+    private Main() {
+
+    }
+
+    /**
+     * Main starting point for the app
+     *
+     * @param args - not used.
+     */
+    public static void main(final String[] args) {
+
+    }
+}
+
